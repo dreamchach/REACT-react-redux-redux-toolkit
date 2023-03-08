99a4176e8d72dc7fd15d14c308ed602465259b86
Kim JiYeong <zmswkfl@naver.com>
2023-03-08 20:01:46 +0900

    feat: redux-toolkit 연습

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => {
+    return state.counter.value;
+  });
+
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}>
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}>
+        -1
+      </button>
+      <button
+        onClick={() => {
+          dispatch(incrementByAmount(5));
+        }}>
+        +5
+      </button>
+      <button
+        onClick={() => {
+          dispatch(fetchIncrement());
+        }}>
+        Fetch Increment
+      </button>
+      <div>{count}</div>
+    </div>
+  );
+};
+
+export default Counter;
